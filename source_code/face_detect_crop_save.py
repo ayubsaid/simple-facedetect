6bbf1a7bb46b30044fb5e0cd65a36fcfe8435215
ayub said <ayubayubsaid@gmail.com>
2023-07-06 15:36:03 +0500

    add the time limit 'datatime'

--- source_code/face_detect_crop_save.py
+++ source_code/face_detect_crop_save.py
@@ -1,17 +1,22 @@
 import cv2
 import os
-import datetime 
+import datetime
 from simple_facerec import SimpleFacerec
 
 images_folder = 'images1/'
+time_limit = datetime.timedelta(seconds=20)
 
 # Encode faces from a folder
 sfr = SimpleFacerec()
 sfr.load_encoding_images("face_database/")
 
 # Load Camera
-cap = cv2.VideoCapture()  # Set the appropriate device index or video file path
-cap.open("rtsp://admin:asd123()@192.168.0.64:554/Streaming/channels/2/")
+cap = cv2.VideoCapture(0)  # Set the appropriate device index or video file path
+# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)  # Set the desired width
+# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)  # Set the desired height
+
+
+known_faces = {}  # Dictionary to store the names and last detection times of already detected faces
 
 while True:
     ret, frame = cap.read()
@@ -19,28 +24,58 @@ while True:
     # Perform face recognition
     face_locations, face_names = sfr.detect_known_faces_tol(frame, tolerance=0.50)
 
+    current_time = datetime.datetime.now()
+
     for (top, right, bottom, left), name in zip(face_locations, face_names):
         if not name:
-            continue
+            name = 'Unknown'
 
         # Draw a rectangle around the face
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-        
+
         # Create folder if it doesn't exist
         folder_path = os.path.join(images_folder, name)
         if not os.path.exists(folder_path):
             os.makedirs(folder_path)
 
-        # Generate filename with timestamp
-        timestamp = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-        filename = os.path.join(folder_path, f"{name}-{timestamp}.jpg")
+        # Check if the face is already known and detected
+        if name in known_faces:
+            last_detection_time = known_faces[name]
+            time_difference = current_time - last_detection_time
 
-        # Crop the face region
-        crop_img = frame[top:bottom, left:right]
+            # Check if the face has not been detected within the time limit
+            if time_difference >= time_limit:
+                # Generate filename with timestamp
+                timestamp = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+                filename = os.path.join(folder_path, f"{name}-{timestamp}.jpg")
 
-        # Save the cropped image
-        cv2.imwrite(filename, crop_img)
-        print(f"Saved cropped face to: {filename}")
+                # Crop the face region
+                crop_img = frame[top:bottom, left:right]
+
+                # Save the cropped image
+                cv2.imwrite(filename, crop_img)
+                print(f"Saved updated face image to: {filename}")
+
+                # Update the last detection time for the face
+                known_faces[name] = current_time
+
+        elif name == 'Unknown':
+            # Generate filename with timestamp
+            timestamp = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+            filename = os.path.join(folder_path, f"{name}-{timestamp}.jpg")
+
+            # Crop the face region
+            crop_img = frame[top:bottom, left:right]
+
+            # Save the cropped image
+            cv2.imwrite(filename, crop_img)
+            print(f"Saved new face image to: {filename}")
+
+            # Add the face and detection time to the known_faces dictionary
+            known_faces[name] = current_time
+
+        # Display the name on the rectangle
+        cv2.putText(frame, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 255, 255), 1)
 
     cv2.imshow("Frame", frame)
 
@@ -50,4 +85,3 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
-
